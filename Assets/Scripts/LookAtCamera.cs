ace4f329a657fd425c0693cae131ac48e612d737
lollipop.et <truonglehiep@gmail.com>
2023-04-17 17:42:54 +0700

    adding more Modes for camera facing

--- Assets/Scripts/LookAtCamera.cs
+++ Assets/Scripts/LookAtCamera.cs
@@ -1,10 +1,35 @@
 using UnityEngine;
 
 public class LookAtCamera : MonoBehaviour {
+    private enum Mode {
+        LookAt,
+        LookAtInverted,
+        CameraForward,
+        CameraForwardInverted
+    }
+
+    [SerializeField] private Mode mode;
 
     // This Camera is already cached by default, so it wont loop through the objects
     // to get the Camera every frame
     private void LateUpdate() {
-        transform.LookAt(Camera.main.transform);
+        switch (mode) {
+            case Mode.LookAt:
+                transform.LookAt(Camera.main.transform);
+                break;
+            case Mode.LookAtInverted:
+                var dirFromCamera = transform.position - Camera.main.transform.position;
+                transform.LookAt(transform.position + dirFromCamera);
+                break;
+            case Mode.CameraForward:
+                transform.forward = (Camera.main.transform.forward);
+                break;
+            case Mode.CameraForwardInverted:
+                transform.forward = (-Camera.main.transform.forward);
+                break;
+
+        }
+
+
     }
 }
